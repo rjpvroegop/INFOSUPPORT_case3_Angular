177c43eca8b3a3610d23ffd402d1ef23193f5848
Yusuf <info@javaminor2015.com>
2017-01-11 12:27:32 +0100

    Collapsible forms for addresses

--- app/pages/customer-overview/customer-overview.component.ts
+++ app/pages/customer-overview/customer-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {Customer} from "../../model/Customer";
+import {materialize} from "rxjs/operator/materialize";
 @Component({
   moduleId: module.id,
   selector : 'customer-overview',
@@ -7,21 +8,17 @@ import { Component } from '@angular/core';
 })
 export class CustomerOverviewComponent {
   title = "Overview Of Customers";
-  customer = new Customer();
+  customer : Customer = {
+    firstName : "Yusuf",
+    addresses : [{street : "Sesamstraat 1"}, {street : "Sesamstraat 2"}, {street : "Sesamstraat 3"}, {street : "Sesamstraat 4"}]
+  };
+
+  ngAfterViewInit(){
+    Materialize.updateTextFields();
+    $('.collapsible').collapsible();
+  }
 
   clicked(){
     console.log(this.customer);
   }
-}
-
-
-class Customer {
-  id : number;
-  firstName : string;
-  lastName : string;
-  birthDate : any;
-  address : any;
-  credit : number;
-  email : string;
-  phone : string
 }
