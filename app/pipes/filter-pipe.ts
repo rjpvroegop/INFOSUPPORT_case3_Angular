b14ca770663e9c4314c65bb3320eed09ec9b6a73
Randy <randy@vroegop.pro>
2017-01-11 14:12:48 +0100

    sidebar filter options

--- app/pipes/filter-pipe.ts
+++ app/pipes/filter-pipe.ts
@@ -4,13 +4,19 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: "category"
 })
 export class FilterPipe implements PipeTransform {
-  transform(values: any, key_title: string): any[] {
+  transform(values: any, bikes: boolean, parts: boolean, clothes: boolean, misc: boolean): any[] {
     return values.filter((item) => {
       let isValidForThisFilter = false;
 
       item.categoryList.forEach(category => {
-        if(category.id == key_title){
-          isValidForThisFilter = true;
+        if (category.id == 1) {
+          isValidForThisFilter = bikes;
+        } else if (category.id == 2) {
+          isValidForThisFilter = parts;
+        } else if (category.id == 3) {
+          isValidForThisFilter = clothes;
+        } else if (category.id == 4) {
+          isValidForThisFilter = misc;
         }
       });
 
